7aee1c91a62ed0a930ca60a030a39f6b909edce1
Lucian <lucianw9977@gmail.com>
2023-03-23 18:07:27 -0700

    namespace

--- app.js
+++ app.js
@@ -1,71 +1,67 @@
-const app = {};
+// Create a namespace object
+const MovieApp = {
+  likedMovies: [],
+  firstLike:true,
+  maxLikedMovies: 5,
 
-// Declare global variables
-let likedMovies = []; // Array to hold liked movies
-const maxLikedMovies = 5; // Maximum number of liked movies
+  fetchMovie: function() {
+    const apiKey = "a86709241fa3002625b118e87d177b48";
+    const baseUrl = "https://api.themoviedb.org/3";
+    const page = Math.floor(Math.random() * 500);
+    const url = `${baseUrl}/movie/popular?api_key=${apiKey}&language=en-US&page=${page}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => this.displayMovie(data.results[0]))
+      .catch(error => console.log(error)); // Handle errors
+  },
 
-// Function to fetch movie data from API
-function fetchMovie() {
-  const apiKey = "a86709241fa3002625b118e87d177b48";
-  const baseUrl = "https://api.themoviedb.org/3";
-  const page = Math.floor(Math.random() * 500);
-  const url = `${baseUrl}/movie/popular?api_key=${apiKey}&language=en-US&page=${page}`;
-  fetch(url)
-    .then(response => response.json())
-    .then(data => displayMovie(data.results[0]))
-    .catch(error => console.log(error)); // Handle errors
-}
+  displayMovie: function(movie) {
+    const imageContainer = document.querySelector(".imageContainer");
+    const posterUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    const title = movie.title;
+    const image = document.createElement("img"); // Create an <img> element
+    image.src = posterUrl; // Set the source of the <img> element
+    image.alt = `${title} poster`; // Set the alt text of the <img> element
+    imageContainer.innerHTML = ""; // Clear the contents of the image container
+    imageContainer.appendChild(image); // Add the <img> element to the image container
+    document.querySelector(".movieName").textContent = title;
+  },
 
-// Function to display movie data
-function displayMovie(movie) {
-  const imageContainer = document.querySelector(".imageContainer");
-  const posterUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-  const title = movie.title;
-  const image = document.createElement("img"); // Create an <img> element
-  image.src = posterUrl; // Set the source of the <img> element
-  image.alt = `${title} poster`; // Set the alt text of the <img> element
-  imageContainer.innerHTML = ""; // Clear the contents of the image container
-  imageContainer.appendChild(image); // Add the <img> element to the image container
-  document.querySelector(".movieName").textContent = title;
-}
-
-// Function to add movie to liked movies list
-function addLikedMovie(movie) {
-  let isDuplicate = false;
-  for (let i = 0; i < likedMovies.length; i++) {
-    if (likedMovies[i].title === movie.title) {
-      isDuplicate = true;
-      break;
+  addLikedMovie: function(movie) {
+    let isDuplicate = false;
+    for (let i = 0; i < this.likedMovies.length; i++) {
+      if (this.likedMovies[i].title === movie.title) {
+        isDuplicate = true;
+        break;
+      }
     }
-  }
-  if (!isDuplicate && likedMovies.length < maxLikedMovies) {
-    likedMovies.push(movie);
-    const likedMoviesList = document.querySelector("#likedMovies");
-    const listItem = document.createElement("li");
-    listItem.textContent = movie.title;
-    likedMoviesList.appendChild(listItem);
+    if (!isDuplicate && this.likedMovies.length < this.maxLikedMovies) {
+      this.likedMovies.push(movie);
+      const likedMoviesList = document.querySelector("#likedMovies");
+      const listItem = document.createElement("li");
+      listItem.textContent = movie.title;
+      likedMoviesList.appendChild(listItem);
 
-    if (likedMovies.length === maxLikedMovies) {
-      alert("You have liked 5 movies. You cannot like any more movies.");
-      const likeButton = document.querySelector(".like");
-      likeButton.disabled = true;
+      if (this.likedMovies.length === this.maxLikedMovies) {
+        alert("You have liked 5 movies. You cannot like any more movies.");
+        const likeButton = document.querySelector(".like");
+        likeButton.disabled = true;
+      }
     }
-  }
-}
+  },
 
-
-// Function to remove movie from suggestions
-function removeMovie() {
-  const imageContainer = document.querySelector(".imageContainer");
-  imageContainer.innerHTML = "";
-  document.querySelector(".movieName").textContent = "";
-}
+  removeMovie: function() {
+    const imageContainer = document.querySelector(".imageContainer");
+    imageContainer.innerHTML = "";
+    document.querySelector(".movieName").textContent = "";
+  },
+};
 
 // Event listener for dislike button
 const dislikeButton = document.querySelector(".dislike");
 dislikeButton.addEventListener("click", function() {
-  removeMovie();
-  fetchMovie();
+  MovieApp.removeMovie();
+  MovieApp.fetchMovie();
 });
 
 // Event listener for like button
@@ -73,25 +69,66 @@ const likeButton = document.querySelector(".like");
 likeButton.addEventListener("click", function() {
   const currentMovie = document.querySelector(".movieName").textContent;
   const movieObj = { title: currentMovie };
-  addLikedMovie(movieObj);
-  removeMovie();
-  fetchMovie();
+  MovieApp.addLikedMovie(movieObj);
+  MovieApp.removeMovie();
+  MovieApp.fetchMovie();
 });
+
 
 // Initial fetch
-fetchMovie();
+MovieApp.fetchMovie();
 
 // slide out menu code:
+const app = {};
 
 // select the open and close menu buttons
-const openMenuButton = document.getElementById('openButton');
-const closeMenuButton = document.getElementById('closeButton');
+app.openMenuButton = document.getElementById('openButton');
+app.closeMenuButton = document.getElementById('closeButton');
 
-openMenuButton.addEventListener("click", () => {
-  document.getElementById("slideoutMenu").style.width = "300px";
-});
+// value that keeps track of whether the menu is open or not
+// 
+app.isOpen = true;
 
-// when close menu button is pressed the slide out menu is closed
-closeMenuButton.addEventListener("click", () => {
+app.menuWidth = 0;
+
+// function that closes the menu by setting its width to zero
+app.closeMenu = function() {
   document.getElementById("slideoutMenu").style.width = "0";
-});
+  app.isOpen = false;
+}
+
+app.mediaQuery = function () {
+  let mq = window.matchMedia('(max-width: 700px)');
+  // if screen size is less than 700px
+  if (mq.matches) {
+    app.openMenuButton.addEventListener("click", () => {
+      document.getElementById("slideoutMenu").style.width = "100vw";
+    });
+
+    // when close menu button is pressed the slide out menu is closed
+    app.closeMenuButton.addEventListener("click", () => {
+      app.closeMenu();
+    });
+    
+
+    // if screen size is greater than 700px
+  } else {
+    app.openMenuButton.addEventListener("click", () => {
+      document.getElementById("slideoutMenu").style.width = "300px";
+    });
+
+    // when close menu button is pressed the slide out menu is closed
+    app.closeMenuButton.addEventListener("click", () => {
+      app.closeMenu();
+    });
+    
+  }
+}
+
+// what to do when menu resizes
+window.addEventListener('resize', function (event) {
+  document.getElementById("slideoutMenu").style.width = "0";
+  app.mediaQuery();
+}, true);
+
+app.mediaQuery();
